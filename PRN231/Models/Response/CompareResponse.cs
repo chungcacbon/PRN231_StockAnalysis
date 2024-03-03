b3675cc5c458a3cf8effdbd855fa923749660d34
Đức Minh <minh3ehqz@gmail.com>
2024-03-03 17:53:10 +0700

    update

---
+++ PRN231/Models/Response/CompareResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PRN231.Models.Response
+{
+    public class CompareResponse
+    {
+        public string Sym { get; set; }
+        public string s { get; set; }
+        public List<int> t { get; set; }
+        public List<double> c { get; set; }
+        public List<double> o { get; set; }
+        public List<double> h { get; set; }
+        public List<double> l { get; set; }
+        public List<int> v { get; set; }
+
+    }
+}
