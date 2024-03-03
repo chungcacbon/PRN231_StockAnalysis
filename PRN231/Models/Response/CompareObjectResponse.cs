82cfbd1996e0079dc7c9c029cb1a8f8b3a831fe5
Đức Minh <minh3ehqz@gmail.com>
2024-03-03 21:52:51 +0700

    update

--- PRN231/Models/Response/CompareObjectResponse.cs
+++ PRN231/Models/Response/CompareObjectResponse.cs
@@ -6,13 +6,13 @@ namespace PRN231.Models.Response
     {
         public DateTime Time { get; set; }
 
-        public double Close { get; set; }
 
         public double Open { get; set; }
 
         public double Hight { get; set; }
 
         public double Low { get; set; }
+        public double Close { get; set; }
         public int Volume { get; set; }
     }
 }
