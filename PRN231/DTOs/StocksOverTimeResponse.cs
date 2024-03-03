42131ca2645a71e14ba49a2c95b3feb5d2b0c620
ThienThang02 <nguyenthienthang2002fw@gmail.com>
2024-03-03 21:53:56 +0700

    update get by id ver 2

---
+++ PRN231/DTOs/StocksOverTimeResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PRN231.DTOs
+{
+	public class StocksOverTimeResponse
+	{
+		public long Time { get; set; }
+		public double Open { get; set; }
+		public double High { get; set; }
+		public double Low { get; set; }
+		public double Close { get; set; }
+		public long Volume { get; set; }
+	}
+}
